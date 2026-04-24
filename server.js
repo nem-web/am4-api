6c67d21ed230d88667fa7c1f63b86900f3591d60
IronLeaf <146615281+nem-web@users.noreply.github.com>
2026-04-25 01:27:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.get("/run", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
